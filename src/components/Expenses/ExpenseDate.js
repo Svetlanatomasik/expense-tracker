2891f50dc108ba161a7e3741e0f73610569d398b
Svetlana Tomašik <seka.corazon@gmail.com>
2023-01-16 14:22:22 +0100

    changing functions into arrow functions

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,7 +1,7 @@
 //this component will rendering the date of an expense
 import "./ExpenseDate.css";
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-us", { day: "2-digit" });
   const year = props.date.getFullYear();
@@ -13,6 +13,6 @@ function ExpenseDate(props) {
       <div className="expense-date__day">{day}</div>
     </div>
   );
-}
+};
 
 export default ExpenseDate;

7e092e919dddbcfbde14ae0957758713546d7988
Svetlana Tomašik <seka.corazon@gmail.com>
2023-01-20 10:31:50 +0100

    ternary expression syntax for no expenses found

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -22,14 +22,19 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
